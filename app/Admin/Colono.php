15e56a9d0f3d0b6f0b20300728f74a186f99635b
jesusrv1103 <jesus21c@hotmail.com>
2019-10-19 22:01:05 -0500

    Migraciones antes de estadisticas

---
+++ app/Admin/Colono.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace BUHWAR\Admin;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Colono extends Model
+{
+    //
+}
---
+++ database/migrations/2019_10_19_230850_create_planes_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePlanesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('planes', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nombre');
+            $table->integer('numero_not_semanales');
+            $table->boolean('visualizar_perfilGuardia');
+            $table->boolean('visualizar_cambioTurno');
+            $table->boolean('visualizacion_estadisticas');
+            $table->boolean('boton_notificador_emergencias');
+            $table->boolean('visualizador_visitas');
+            $table->boolean('valoracion_desempeio_guardias');
+            $table->int('numero_usuario_x_plan');
+            $table->boolean('notficacion_rondin');
+            $table->int('numero_vizualizaciones_rondin');
+            $table->int('numero_codigo_qr');
+            $table->boolean('estado');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('planes');
+    }
+}
